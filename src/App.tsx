2126929511bd8366adc1a93de8e02e0cc8a43fb4
caique.fuzaite <caiqueandrey@hotmail.com>
2021-12-23 14:58:56 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyle from './styles/GlobalStyle';
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle/>
+    </>
+  );
+}
+
+export default App;
